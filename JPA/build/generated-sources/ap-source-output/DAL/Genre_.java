11cf71cffde35e4af2f1b5d35bc612c649cb2452
RobinVanRoy <vanroyrobin@gmail.com>
2015-01-14 16:03:50 +0100

    JDK verandert van 8 naar 7

--- JPA/build/generated-sources/ap-source-output/DAL/Genre_.java
+++ JPA/build/generated-sources/ap-source-output/DAL/Genre_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-13T16:56:39")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-01-14T15:46:58")
 @StaticMetamodel(Genre.class)
 public class Genre_ { 
 
--- Login/build/generated-sources/ap-source-output/DAL/Login_.java
+++ Login/build/generated-sources/ap-source-output/DAL/Login_.java
@@ -4,12 +4,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-13T16:56:42")
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-01-14T15:47:02")
 @StaticMetamodel(Login.class)
 public class Login_ { 
 
-    public static volatile SingularAttribute<Login, String> password;
     public static volatile SingularAttribute<Login, Integer> id;
     public static volatile SingularAttribute<Login, String> userName;
+    public static volatile SingularAttribute<Login, String> password;
 
 }
