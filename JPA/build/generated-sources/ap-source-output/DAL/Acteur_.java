da3771d4325218c360f4ff5b2e6c04cc0db9652b
RobinVanRoy <vanroyrobin@gmail.com>
2015-01-15 21:56:59 +0100

    geen idee

--- JPA/build/generated-sources/ap-source-output/DAL/Acteur_.java
+++ JPA/build/generated-sources/ap-source-output/DAL/Acteur_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2015-01-15T14:03:13")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-15T20:12:23")
 @StaticMetamodel(Acteur.class)
 public class Acteur_ { 
 
