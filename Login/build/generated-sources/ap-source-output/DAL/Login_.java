8277fdf836965c0f4512a39512f8027a8f5c1d4b
RobinVanRoy <vanroyrobin@gmail.com>
2015-01-16 15:52:11 +0100

    Clean and Build

---
+++ Login/build/generated-sources/ap-source-output/DAL/Login_.java
@@ -0,0 +1,15 @@
+package DAL;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-16T15:51:36")
+@StaticMetamodel(Login.class)
+public class Login_ { 
+
+    public static volatile SingularAttribute<Login, String> password;
+    public static volatile SingularAttribute<Login, Integer> id;
+    public static volatile SingularAttribute<Login, String> userName;
+
+}
